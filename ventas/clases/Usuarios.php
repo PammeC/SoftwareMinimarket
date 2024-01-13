d3bae18e527d986f47d63dea97dcc05acab8a732
AlisonAlbuja <aaalbujaa@uce.edu.ec>
2024-01-13 13:49:08 -0500

    Modifique la clase usuario

--- ventas/clases/Usuarios.php
+++ ventas/clases/Usuarios.php
@@ -54,7 +54,50 @@
 			return mysqli_fetch_row($result)[0];
 		}
 
-		
+		public function obtenDatosUsuario($idusuario){
+
+			$c=new conectar();
+			$conexion=$c->conexion();
+
+			$sql="SELECT id_usuario,
+							nombre,
+							apellido,
+							email
+					from usuarios 
+					where id_usuario='$idusuario'";
+			$result=mysqli_query($conexion,$sql);
+
+			$ver=mysqli_fetch_row($result);
+
+			$datos=array(
+						'id_usuario' => $ver[0],
+							'nombre' => $ver[1],
+							'apellido' => $ver[2],
+							'email' => $ver[3]
+						);
+
+			return $datos;
+		}
+
+		public function actualizaUsuario($datos){
+			$c=new conectar();
+			$conexion=$c->conexion();
+
+			$sql="UPDATE usuarios set nombre='$datos[1]',
+									apellido='$datos[2]',
+									email='$datos[3]'
+						where id_usuario='$datos[0]'";
+			return mysqli_query($conexion,$sql);	
+		}
+
+		public function eliminaUsuario($idusuario){
+			$c=new conectar();
+			$conexion=$c->conexion();
+
+			$sql="DELETE from usuarios 
+					where id_usuario='$idusuario'";
+			return mysqli_query($conexion,$sql);
+		}
 	}
 
  ?>
