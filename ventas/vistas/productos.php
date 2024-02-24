124c1610f5fadae1f348d263219bf579a00a49c1
PammeC <pamelajnth@gmail.com>
2024-02-24 11:23:34 -0500

    Arreglo interfaz productos

--- ventas/vistas/productos.php
+++ ventas/vistas/productos.php
@@ -19,8 +19,31 @@ if(isset($_SESSION['usuario'])){
 			<div class="container">
 				<h1>Productos</h1>
 				<div class="row">
-					<div class="col-sm-14">
-						<span id="btnOrdenarCategoria" class="btn btn-primary">Ordenar por Categoria</span>
+					<div class="col-sm-4">												
+						<form id="frmArticulos" enctype="multipart/form-data">
+							<label>Categoria</label>
+							<select class="form-control input-sm" id="categoriaSelect" name="categoriaSelect">
+								<option value="A">Selecciona Categoria</option>
+								<?php while($ver=mysqli_fetch_row($result)): ?>
+									<option value="<?php echo $ver[0] ?>"><?php echo $ver[1]; ?></option>
+								<?php endwhile; ?>
+							</select>
+							<label>Nombre</label>
+							<input type="text" class="form-control input-sm" id="nombre" name="nombre">
+							<label>Descripcion</label>
+							<input type="text" class="form-control input-sm" id="descripcion" name="descripcion">
+							<label>Cantidad</label>
+							<input type="text" class="form-control input-sm" id="cantidad" name="cantidad">
+							<label>Precio</label>
+							<input type="text" class="form-control input-sm" id="precio" name="precio">
+							<label>Imagen</label>
+							<input type="file" id="imagen" name="imagen">
+							<p></p>
+							<span id="btnAgregaArticulo" class="btn btn-primary">Agregar</span>
+						</form>
+					</div>					
+					<div class="col-sm-8">
+					<span id="btnOrdenarCategoria" class="btn btn-primary">Ordenar por Categoria</span>
 						<div id="tablaArticulosLoad"></div>
 					</div>
 				</div>
