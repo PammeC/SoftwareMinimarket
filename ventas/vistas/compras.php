e1f119ff636a58a9efc66fb6d4be2e0095331aba
PammeC <pamelajnth@gmail.com>
2024-02-24 12:16:53 -0500

    Arreglo de modulo compras

--- ventas/vistas/compras.php
+++ ventas/vistas/compras.php
@@ -1,49 +1,34 @@
 <?php 
 	session_start();
 	if(isset($_SESSION['usuario'])){
-		
  ?>
- 
 
 <!DOCTYPE html>
 <html>
-<head>
-	<title>compras</title>
-	<?php require_once "menu.php"; ?>
-</head>
-<body>
-
-	<div class="container">
-		 <h1>Compra de productos</h1>
-		 <div class="row">
-		 	<div class="col-sm-12">
-				 <span class="btn btn-default" id="ordenCompraBtn">Orden de Compra</span>
-		 		<span class="btn btn-default" id="compraProductosBtn">Comprar producto</span>
-		 		<span class="btn btn-default" id="comprasHechasBtn">Compras hechas</span>
-				 <span class="btn btn-default" id="devolucionesBtn">Devoluciones</span>
-		 	</div>
-		 </div>
-		 <div class="row">
-		 	<div class="col-sm-12">			 
-				<div id="ordenCompra"></div>
-		 		<div id="compraProductos"></div>
-		 		<div id="comprasHechas"></div>
-				 <div id="devoluciones"></div>
-		 	</div>
-		 </div>
-	</div>
-</body>
+	<head>
+		<title>compras</title>
+		<?php require_once "menu.php"; ?>
+	</head>
+	<body>
+		<div class="container">
+			<h1>Compra de productos</h1>
+			<div class="row">
+				<div class="col-sm-12">
+					<span class="btn btn-default" id="compraProductosBtn">Comprar producto</span>
+					<span class="btn btn-default" id="comprasHechasBtn">Compras hechas</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">			 
+					<div id="compraProductos"></div>
+					<div id="comprasHechas"></div>
+				</div>
+			</div>
+		</div>
+	</body>
 </html>
-	
 	<script type="text/javascript">
 		$(document).ready(function(){	
-
-
-			$('#ordenCompraBtn').click(function(){
-				esconderSeccionCompra();
-				$('#ordenCompra').load('ordenCompra.php');
-				$('#ordenCompra').show();
-			});
 
 			$('#compraProductosBtn').click(function(){
 				esconderSeccionCompra();
@@ -55,23 +40,12 @@
 				$('#comprasHechas').load('compras/comprasyReportes.php');
 				$('#comprasHechas').show();
 			});
-
-			$('#devolucionesBtn').click(function(){
-				esconderSeccionCompra();
-				$('#devoluciones').load('devoluciones.php');
-				$('#devoluciones').show();
-			});
 		});
-
 		function esconderSeccionCompra(){			
-			$('#ordenCompra').hide();
 			$('#compraProductos').hide();
 			$('#comprasHechas').hide();
-			$('#devoluciones').hide();
 		}
-
 	</script>
-
 <?php 
 	}else{
 		header("location:../index.php");
